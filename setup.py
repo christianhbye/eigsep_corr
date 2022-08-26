b1887df3c793333b27017fe1c1485e6e1f18215b
Christian Hellum Bye <chbye@berkeley.edu>
2022-08-26 16:19:40 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='eigsep_corr',
+    version='0.0.1',
+    url='https://github.com/christianhbye/eigsep_corr',
+    author='Christian Hellum Bye',
+    author_email='chbye@berkeley.edu',
+    description='Software to control EIGSEP correlator on SNAP',
+    packages=find_packages(), 
+)
